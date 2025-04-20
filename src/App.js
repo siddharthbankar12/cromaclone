a5b3bf40f085389efb104b4b4b6b7f05e440ae85
siddharthbankar12 <siddharthbankar1204@gmail.com>
2025-04-21 00:37:27 +0530

    updates

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
